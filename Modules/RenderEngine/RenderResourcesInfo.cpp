088b53ad7ba4a77b2ee8e17f3fe4c1be3e0a9e25
Artem Vetik <av.unitygames@gmail.com>
2025-01-02 20:40:48 +0300

    Add lights info in RenderResourcesInfo window

--- Modules/RenderEngine/RenderResourcesInfo.cpp
+++ Modules/RenderEngine/RenderResourcesInfo.cpp
@@ -77,6 +77,14 @@ namespace EduEngine
 			m_Oss << "\tPtr:\t" << camera.get() << "\n\n";
 		}
 
+		m_Oss << separator;
+		m_Oss << "Lights (" << m_RenderEngine->m_Lights.size() << "):\n";
+		for (auto& light : m_RenderEngine->m_Lights)
+		{
+			m_Oss << "\tPtr:\t" << light.get() << "\n\n";
+			m_Oss << "\tType:\t" << light->LightType << "\n\n";
+		}
+
 		m_Oss << "::EDITOR::\n\n";
 
 		if (!onlyUsed || m_EditorRenderEngine->m_PreviewTex.get())
