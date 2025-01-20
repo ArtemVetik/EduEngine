ca0f5ffa12e9fae3e81e051e72b1c78826b29a6a
Artem Vetik <av.unitygames@gmail.com>
2025-01-21 01:36:40 +0300

    Parse cmdLine arguments for folderPath and dllPath

--- EduEngine/main.cpp
+++ EduEngine/main.cpp
@@ -1,19 +1,34 @@
 #include "EduEngine.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-	PSTR cmdLine, int showCmd)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
 {
 #if defined(DEBUG) | defined(_DEBUG)
 	EduEngine::Common::AllocWinConsole();
 #endif
 
-	std::wstring folderPath = EduEngine::Common::OpenFolderDialog(true, L"Select Assets folder");
-	if (folderPath.empty())
-		return 0;
+	std::wstring folderPath;
+	std::wstring dllPath;
 
-	std::wstring dllPath = EduEngine::Common::OpenFolderDialog(false, L"Select scripts DLL file");
-	if (dllPath.empty())
-		return 0;
+	int argc = 0;
+	LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+	if (argc >= 3)
+	{
+		folderPath = argv[1];
+		dllPath = argv[2];
+	}
+	LocalFree(argv);
+
+	if (argc < 3)
+	{
+		folderPath = EduEngine::Common::OpenFolderDialog(true, L"Select Assets folder");
+		if (folderPath.empty())
+			return 0;
+
+		dllPath = EduEngine::Common::OpenFolderDialog(false, L"Select scripts DLL file");
+		if (dllPath.empty())
+			return 0;
+	}
 
 	EduEngine::EduEngine engine(hInstance, folderPath, dllPath);
 	engine.Run();
